8e6394f0259a22eb9af8aab5f08d410519e3c940
Shuhei Suzuki <s.shuhei0207@gmail.com>
2021-12-22 00:03:18 +0900

    deploy build file

---
+++ build/dist/index.js
@@ -0,0 +1,6 @@
+import React from "../_snowpack/pkg/react.js";
+import ReactDOM from "../_snowpack/pkg/react-dom.js";
+const App = () => {
+  return /* @__PURE__ */ React.createElement("div", null, "Hello world!");
+};
+ReactDOM.render(/* @__PURE__ */ React.createElement(App, null), document.getElementById("app"));
